6fa30a42b9ac733b24f49a0088e51fc000c6d530
samimaat <saminiksi@gmail.com>
2022-04-29 08:02:33 +0300

    Add javacsript and try to use an API

---
+++ index/index.js
@@ -0,0 +1,25 @@
+// fetch("http://localhost:8080/swagger-ui/index.html#/restaurant-controller/fetchRestaurants")
+//     .then(response => response.json())
+//     .then(restaurants => showRestaurants(restaurants.results));
+
+// showRestaurants = restaurants => {
+//     const restaurantsDiv = document.querySelector("#restaurant-data");
+
+//     restaurants.forEach(restaurant => {
+//         const restaurantElement = document.createElement("p");
+//         restaurantElement.innerText = "Restaurant Name: ${restaurant.name}"
+//     });
+// }
+
+fetch("https://rickandmortyapi.com/api/character/")
+   .then(response => response.json())
+   .then(characters => showCharacters(characters.results));
+
+showCharacters = characters => {
+    const charactersDiv = document.querySelector("#character-data");
+    characters.forEach(character => {
+      const characterElement = document.createElement("p");
+      characterElement.innerText = "Character Name: ${character.name}";
+      charactersDiv.append(characterElement);
+    });
+  }
