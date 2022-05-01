18900e393350a4dea9d6018477b74566b9510b80
samimaat <saminiksi@gmail.com>
2022-05-01 11:58:31 +0300

    Turn global variables into locals

--- index/test.js
+++ index/test.js
@@ -1,15 +1,17 @@
 // Rick and Morty API
 // https://jack72828383883.medium.com/how-to-use-javascript-fetch-to-display-api-results-in-html-7aa59936ed30
 
-let i = 0;
+
 
 fetch("https://rickandmortyapi.com/api/character/")
    .then(response => response.json())
    .then(characters => showCharacters(characters.results));
 
 showCharacters = characters => {
+    let i = 0;
+
     const charactersForm = document.getElementById("character-data");
-    console.log(characters);
+    // console.log(characters);
     
 
     characters.forEach(character => {
@@ -34,7 +36,6 @@ showCharacters = characters => {
       charactersForm.append(newline);
 
       i++;
-      console.log(i);
     });
   }
 
