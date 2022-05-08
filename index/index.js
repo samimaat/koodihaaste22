35e1012a0087a53a52cdcf460242241aed8ba856
samimaat <saminiksi@gmail.com>
2022-05-08 19:51:17 +0300

    Add voting button

--- index/index.js
+++ index/index.js
@@ -9,27 +9,26 @@ const api_url = "http://localhost:8080/api/v1/restaurants/"
 // Fetches the restaurant information from the API and continues into a function, which constructs the HTML and displays on the page.
 
 function fetchAndShowRestaurants(e) {
-    e.preventDefault();
-    console.log("Form!")
-    // This doesn't work! Why can't it not submi?!??!
-    // MAybe this helps? -> https://www.youtube.com/watch?v=I_fVO_NzT2g
+    e.preventDefault(); // Prevents the website from updating and swallowing the search result.
+
+    // This helped me with the next line: https://www.youtube.com/watch?v=S944-epyYuI
+    // This also had the answer, but I didn't know how to put 2 and 2 together: https://stackoverflow.com/questions/59060113/sending-data-from-a-search-bar-to-a-url
+
     let city = document.getElementById("city").value;
-    console.log(city);
+    // console.log(city);
 
     fetch(api_url + city)
         .then(response => response.json())
         .then(restaurants => showRestaurants(restaurants));
 
-// Might help: https://stackoverflow.com/questions/59060113/sending-data-from-a-search-bar-to-a-url
-
-// Loops and returns all the restaurant names in a specific city.
+    // Loops and returns all the restaurant names in a specific city.
     showRestaurants = restaurantsAPI => {
         let i = 0;
-        
+
         const restaurantsForm = document.getElementById("restaurant-data");
 
         restaurantsAPI.restaurants.forEach(restaurant => {
-            
+
             const restaurantId = "restaurant" + i;
 
             const restaurantElement = document.createElement("input");
@@ -49,20 +48,28 @@ function fetchAndShowRestaurants(e) {
             restaurantsForm.append(restaurantElement, label, newline);
 
             i++; // Incerement ID number
-            
-    });
-}
+        })
+
+        // Generating a voting button. Needs functionality!
+        const voteSubmit = document.createElement("button");
+        voteSubmit.name="vote_btn";
+        voteSubmit.innerText="Äänestä"
+
+        const newline = document.createElement("br");
+
+        restaurantsForm.append(voteSubmit, newline);
+    }
 };
 
 const form = document.getElementById("restaurant-search");
 
 form.addEventListener("submit", fetchAndShowRestaurants);
 
-// The function way: 
+// The function way:
 
 // async function getRes() {
 //     const response = await fetch(api_url + city);
-//     const data = await response.json(); // is not a function? 
+//     const data = await response.json(); // is not a function?
 //     // console.log(data)
 //     console.log(data.restaurants);
 //     console.log(data.restaurants[0].name);
