775854169133ea6c11f3b6edc597b1f011ab2a5f
samimaat <saminiksi@gmail.com>
2022-05-08 21:47:15 +0300

    Add bootstrap CSS and improve readability

--- index/index.js
+++ index/index.js
@@ -1,4 +1,6 @@
 const api_url = "http://localhost:8080/api/v1/restaurants/"
+const restaurantDataID = "restaurant-data";
+const form = document.getElementById("restaurant-search");
 
 // The city can be changed
 
@@ -7,6 +9,61 @@ const api_url = "http://localhost:8080/api/v1/restaurants/"
 // Stolen and adapted from https://jack72828383883.medium.com/how-to-use-javascript-fetch-to-display-api-results-in-html-7aa59936ed30. Thanks!
 
 // Fetches the restaurant information from the API and continues into a function, which constructs the HTML and displays on the page.
+
+function fetchRestaurants(city) {
+    fetch(api_url + city)
+        .then(response => response.json())
+        .then(restaurants => showRestaurants(restaurants));
+};
+
+function clearForm(restaurantDataID) {
+    document.getElementById(restaurantDataID).innerHTML = "";
+}
+
+// Loops and returns all the restaurant names in a specific city.
+function showRestaurants(restaurantsAPI) {
+    // Format the form at the start of each loop.
+    clearForm(restaurantDataID);
+
+    let i = 0;
+
+    const restaurantsForm = document.getElementById(restaurantDataID);
+
+    restaurantsAPI.restaurants.forEach(restaurant => {
+
+        const restaurantId = "restaurant" + i;
+
+        const restaurantRadioButton = document.createElement("input");
+        restaurantRadioButton.className = "form-check-input";
+        restaurantRadioButton.type = "radio";
+        restaurantRadioButton.name = "restaurant_voting";
+        restaurantRadioButton.id = restaurantId;
+        restaurantRadioButton.value = "restaurant_option";
+
+        const restaurantRadioLabel = document.createElement("label");
+        restaurantRadioLabel.className = "form-check-label";
+        restaurantRadioLabel.htmlFor = restaurantId;
+
+        const restaurantName = document.createTextNode(restaurant.name);
+        restaurantRadioLabel.append(restaurantName);
+
+        const newline = document.createElement("br");
+
+        restaurantsForm.append(restaurantRadioButton, restaurantRadioLabel, newline);
+
+        i++; // Incerement ID number
+    })
+
+    // Generating a voting button. Needs functionality!
+    const voteSubmit = document.createElement("button");
+    voteSubmit.name = "vote_btn";
+    voteSubmit.innerText = "Äänestä"
+
+    const newline = document.createElement("br");
+
+    restaurantsForm.append(voteSubmit, newline);
+
+}
 
 function fetchAndShowRestaurants(e) {
     e.preventDefault(); // Prevents the website from updating and swallowing the search result.
@@ -17,82 +74,8 @@ function fetchAndShowRestaurants(e) {
     let city = document.getElementById("city").value;
     // console.log(city);
 
-    fetch(api_url + city)
-        .then(response => response.json())
-        .then(restaurants => showRestaurants(restaurants));
-
-    // Loops and returns all the restaurant names in a specific city.
-    showRestaurants = restaurantsAPI => {
-        let i = 0;
-
-        const restaurantsForm = document.getElementById("restaurant-data");
-
-        restaurantsAPI.restaurants.forEach(restaurant => {
-
-            const restaurantId = "restaurant" + i;
-
-            const restaurantElement = document.createElement("input");
-            restaurantElement.type = "radio";
-            restaurantElement.name = "restaurant_voting";
-            restaurantElement.id = restaurantId;
-            restaurantElement.value = "restaurant_option";
-
-            const label = document.createElement("label");
-            label.htmlFor = restaurantId;
-
-            const description = document.createTextNode("Ravintola: " + restaurant.name);
-            label.append(description);
-
-            const newline = document.createElement("br");
-
-            restaurantsForm.append(restaurantElement, label, newline);
-
-            i++; // Incerement ID number
-        })
-
-        // Generating a voting button. Needs functionality!
-        const voteSubmit = document.createElement("button");
-        voteSubmit.name="vote_btn";
-        voteSubmit.innerText="Äänestä"
-
-        const newline = document.createElement("br");
-
-        restaurantsForm.append(voteSubmit, newline);
-    }
+    fetchRestaurants(city);
 };
 
-const form = document.getElementById("restaurant-search");
-
+// Main function call.
 form.addEventListener("submit", fetchAndShowRestaurants);
-
-// The function way:
-
-// async function getRes() {
-//     const response = await fetch(api_url + city);
-//     const data = await response.json(); // is not a function?
-//     // console.log(data)
-//     console.log(data.restaurants);
-//     console.log(data.restaurants[0].name);
-// }
-
-// getRes();
-
-// Instructions: https://www.youtube.com/watch?v=uxf0--uiX0I
-
-// async function getISS() {
-//     response = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
-//     data = await response.json();
-//     console.log(data)
-// }
-
-// getISS();
-
-// async function getChar2() {
-//     response = await fetch("https://rickandmortyapi.com/api/character/");
-//     data = await response.json();
-//     console.log(data)
-// }
-
-
-// getChar2();
-
